1a1c54ef6dbd40223f89e2be141fc2250b2de3fa
ZaKaizer <aarnoldmarc@gmail.com>
2025-12-01 17:50:19 +0530

    Coin flip

---
+++ terminal_game.py
@@ -0,0 +1,11 @@
+
+def coin_flip(): #Basic coinflip module
+    import random
+    coin = random.randint(1,2)
+    if (coin):
+        return("Heads")
+    else:
+        return("Tails")
+    
+
+    
